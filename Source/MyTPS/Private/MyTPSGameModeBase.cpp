749b22853898b8db68c59b8f50fd43d32ecb60ab
jacobyi0730 <78453941+jacobyi0730@users.noreply.github.com>
2022-11-15 11:58:50 +0900

    Initial commit

---
+++ Source/MyTPS/Private/MyTPSGameModeBase.cpp
@@ -0,0 +1,10 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+
+#include "MyTPSGameModeBase.h"
+#include "MyTPS.h"
+
+AMyTPSGameModeBase::AMyTPSGameModeBase()
+{
+	PRINT_LOG(TEXT("Hello %s"), TEXT("World!!"));
+}
