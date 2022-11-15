bca5e38516f3ca878f8ac366aba5ba71bab9f891
jacobyi0730 <78453941+jacobyi0730@users.noreply.github.com>
2022-11-15 14:47:08 +0900

    20221115
    플레이어 설정중

--- Source/MyTPS/Private/TPSPlayer.cpp
+++ Source/MyTPS/Private/TPSPlayer.cpp
@@ -1,7 +1,9 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 
 #include "TPSPlayer.h"
+#include <GameFramework/SpringArmComponent.h>
+#include <Camera/CameraComponent.h>
 
 // Sets default values
 ATPSPlayer::ATPSPlayer()
@@ -9,6 +11,7 @@ ATPSPlayer::ATPSPlayer()
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	// USkeletalMesh(Asset)를 가져와서 내 Mesh에 적용하고싶다.
 	ConstructorHelpers::FObjectFinder<USkeletalMesh> tempBody(TEXT("SkeletalMesh'/Game/Characters/Mannequins/Meshes/SKM_Quinn.SKM_Quinn'"));
 
 	if (tempBody.Succeeded())
@@ -17,6 +20,16 @@ ATPSPlayer::ATPSPlayer()
 		GetMesh()->SetRelativeLocationAndRotation(FVector(0, 0, -90), FRotator(0, -90, 0));
 	}
 
+	// 스프링암 컴포넌트와 카메라 컴포넌트를 생성하고싶다.
+	springArmComp = CreateDefaultSubobject<USpringArmComponent>(TEXT("springArmComp"));
+	cameraComp = CreateDefaultSubobject<UCameraComponent>(TEXT("cameraComp"));
+	// 스프링암은 RootComponent에 Attatch하고싶다.
+	springArmComp->SetupAttachment(RootComponent);
+	// 카메라는 스프링암에 Attatch하고싶다.
+	cameraComp->SetupAttachment(springArmComp);
+	// 스프링암의 위치와 targetArmLength를 설정하고싶다.
+	springArmComp->SetRelativeLocation(FVector(0, 40, 70));
+	springArmComp->TargetArmLength = 100;
 }
 
 // Called when the game starts or when spawned
--- Source/MyTPS/Public/TPSPlayer.h
+++ Source/MyTPS/Public/TPSPlayer.h
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
 
@@ -25,5 +25,12 @@ public:
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	// 스프링암, 카메라를 배치하고싶다.
+	UPROPERTY(EditAnywhere)
+	class USpringArmComponent* springArmComp;
+
+	UPROPERTY(EditAnywhere)
+	class UCameraComponent* cameraComp;
 
 };
