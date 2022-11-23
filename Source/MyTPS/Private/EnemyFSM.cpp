90f2b78826c8eadd53b44d89d3b4598555d5f180
jacobyi <jacobyi0730@gmail.com>
2022-11-23 11:09:17 +0900

    20221123_1100
    EnemyFSM C++ 작업중

---
+++ Source/MyTPS/Private/EnemyFSM.cpp
@@ -0,0 +1,35 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "EnemyFSM.h"
+#include "Enemy.h"
+
+// Sets default values for this component's properties
+UEnemyFSM::UEnemyFSM()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UEnemyFSM::BeginPlay()
+{
+	Super::BeginPlay();
+	state = EEnemyState::Idle;
+	
+	me = Cast<AEnemy>(GetOwner());
+}
+
+
+// Called every frame
+void UEnemyFSM::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
---
+++ Source/MyTPS/Public/EnemyFSM.h
@@ -0,0 +1,44 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "EnemyFSM.generated.h"
+
+UENUM(BlueprintType)
+enum class EEnemyState : uint8
+{
+	Idle UMETA(Displayname= IDLE),
+	Move UMETA(Displayname = MOVE),
+	Attack UMETA(Displayname = ATTACK),
+	Damage UMETA(Displayname = DAMAGE),
+	Die UMETA(Displayname = DIE)
+};
+
+
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class MYTPS_API UEnemyFSM : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UEnemyFSM();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+	UPROPERTY(EditAnywhere)
+	EEnemyState state;
+
+	UPROPERTY()
+	class AEnemy* me;
+};
